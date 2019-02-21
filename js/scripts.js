9820ada246b43c8eee021af87760e94a9717d6ad
Jaccojwang <danolago@gmail.com>
2019-02-21 15:28:18 +0300

    Restructure function to add sum of input

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Business Logic
+$(document).ready(function() {
+  $("form.test").submit(function(event) {
+    // var numbers = ["1","2","3","4","5","6","7","8","9","10","11","12", "13"];
+    // var total = 0;
+
+
+  alert(total);
+
+    event.preventDefault();
+  });
+});
+
+
+// numbers.forEach(function(number) {
+//   var answer = $("input:radio[name=question1]:checked").val();
+//   var total =  total + answer;
+//
+// });
