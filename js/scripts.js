e33819750684760974ba63bc468d033ff61c4508
Jaccojwang <danolago@gmail.com>
2019-02-21 16:43:56 +0300

    Finalise function to calculate score

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 //Business Logic
 $(document).ready(function() {
   $("form.test").submit(function(event) {
-    // var numbers = ["1","2","3","4","5","6","7","8","9","10","11","12", "13"];
-    // var total = 0;
+    var numbers = ["1","2","3","4","5","6","7","8","9","10","11","12"];
+    var score = 0;
+    numbers.forEach(function(number) {
+      var answer = parseInt($("input:radio[name=question" + number + "]:checked").val());
+      score =  score + answer;
+    });
+      alert(score);
 
 
-  alert(total);
 
     event.preventDefault();
   });
 });
 
-
-// numbers.forEach(function(number) {
-//   var answer = $("input:radio[name=question1]:checked").val();
-//   var total =  total + answer;
-//
-// });
+//User Interface Logic
+$(document).ready(function() {
+  $()
+});
