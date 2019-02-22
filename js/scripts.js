adf8cc9b3f94a4d4fa87a583783132bebd30a87f
Jaccojwang <danolago@gmail.com>
2019-02-22 08:32:20 +0300

    Complete function to calculate score as user answers questions

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
     var numbers = ["1","2","3","4","5","6","7","8","9","10","11","12"];
     var score = 0;
     numbers.forEach(function(number) {
+      if (true) {
+
+      } else {
+
+      }
       var answer = parseInt($("input:radio[name=question" + number + "]:checked").val());
       score =  score + answer;
     });
-      alert(score);
-
-
-
+     alert(score);
     event.preventDefault();
   });
 });
