5cdc2ddff4cca35f3aca3a3aff56cc1b072a036f
Jaccojwang <danolago@gmail.com>
2019-02-25 11:06:42 +0300

    Properly separate the UI and UX

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,17 @@ var score = 0;
 var answer = 0;
 var verdict = "";
 var advice = "";
+var percentage = 0;
 //Checks to see how many questions were attempted and how many left blank
 var checkAll = function() {
-    for( i = 0; i < numbers.length; i++) {
-      if($("input:radio[name=question" + numbers[i] + "]").is(":checked")) {
-        answered = answered + 1;
-      }
-      else {
-        blank = blank + 1;
-      }
+  for( i = 0; i < numbers.length; i++) {
+    if($("input:radio[name=question" + numbers[i] + "]").is(":checked")) {
+      answered = answered + 1;
     }
+    else {
+      blank = blank + 1;
+    }
+  }
 };
 //Adds score when radio selected, else doesn't
 var add = function() {
@@ -28,48 +29,54 @@ var add = function() {
   }
 };
 
-var result = function() {
-  if((score/12)  >= (8/10)) {
-    verdict = "This is excellent."
-    advice = "No need to retake the test."
-  }
-  else if((score/12) >= (5/10) && (score/12)  < (8/10)) {
-    verdict = "This is fairly good"
-    advice = "You may retake the test to imptove your score."
-  }
-  else {
-    verdict = "This is a poor score."
-    advice = "It is advisable that you retake the test."
-  }
+//Calculates percentage
+var per = function() {
+  percentage = Math.round(score/12*100);
 };
-
+//Generates final comments
+  var result = function() {
+    if(percentage  >= 80) {
+      verdict = "This is excellent."
+      advice = "No need to retake the test."
+    }
+    else if(percentage >= 50 && percentage  < 80) {
+      verdict = "This is fairly good"
+      advice = "You may retake the test to imptove your score."
+    }
+    else {
+      verdict = "This is a poor score."
+      advice = "It is advisable that you retake the test."
+    }
+};
 
 
 //User Interface Logic
 $(document).ready(function() {
-//(Next button) - display each next question
-function loop() {
-  var questions = ["first","second","third","fourth","fifth","sixth","seventh","eigth","ninth","tenth","eleventh","twelfth"];
-    var i = 0;
-  $('#start').click(function () {
-    $("." + questions[i]).show();
-    $("." + questions[i-1]).hide();
-    i = i + 1 ;
-    });
-}
-loop();
-//(Submit)
+  function loop() {
+    var questions = ["first","second","third","fourth","fifth","sixth","seventh","eigth","ninth","tenth","eleventh","twelfth"];
+      var i = 0;
+    $('#start').click(function () {
+      $("." + questions[i]).show();
+      $("." + questions[i-1]).hide();
+      i = i + 1 ;
+      });
+  }
+  loop();
   $("#form-container").submit(function(event) {
     event.preventDefault();
-//Collects input from user
+    $("#submit").attr("disabled", true);
     numbers.forEach(function(number){
     answer = parseInt($("input:radio[name=question" + number + "]:checked").val());
     add();
     });
-  checkAll();
-  //Returns all final calculations: attempted questions, blanks and score
-  result();
-  alert("You scored " + score + " out of 12" + verdict + advice);
-  //   $("#output").text(verdict);
+    checkAll();
+    result();
+    per();
+    $("#score").text(score + "/12" + " - " + percentage + "%");
+    $("#attempt").text("You attempted " + answered + " question(s) and left " + blank + " blanks.");
+    $("#final").text("You scored " + score + " out of 12. " + verdict + " " + advice);
+  });
+  $('#reload').click(function() {
+    location.reload(true);
   });
 });
